42635b1423b20d2ae26e95bbff45540ec45f9c29
JeongEunBae <je_0922@naver.com>
2023-10-12 00:07:29 +0900

    [BOJ] 2548_대표자연수 / 실버3 / 30분 / O

--- .github/week1/BOJ_2548/대표자연수_배정은.py
+++ .github/week1/BOJ_2548/대표자연수_배정은.py
@@ -1,35 +1,17 @@
 import sys
 
-def difference_number_sum(idx, numbers):
+def difference_number_sum(n, numbers):
     number_sum = 0
     for num in numbers:
-        number_sum += abs(num - numbers[idx]) # 다른 자연수의 차를 모두 더함
+        number_sum += abs(num - n) # 다른 자연수의 차를 모두 더함
     return number_sum
 
 N = int(sys.stdin.readline().rstrip()) # N값 입력
 numbers = sorted(list(map(int, sys.stdin.readline().split()))) # 오름차순으로 정렬하기
 
-# 중앙 위치한 값 인덱스 저장
-left, right = 0, 0
 if len(numbers) % 2 == 1: # 배열 크기가 홀수일 경우에
-    left, right = len(numbers) // 2, len(numbers) // 2
+    print(numbers[len(numbers) // 2]) # 중앙 위치한 값 출력
 else: # 짝수일때
-    left, right = (len(numbers) // 2) - 1, len(numbers) // 2
-
-min_difference_sum = [0, left]
-for idx in range(len(numbers) // 2): # 중간부터 확인
-    left_difference_sum, right_difference_sum = difference_number_sum(left, numbers), difference_number_sum(right, numbers)
-
-    if idx == 0:
-            min_difference_sum = [left_difference_sum, left] if left_difference_sum <= right_difference_sum else [right_difference_sum, right]
-    else:
-        if min_difference_sum[0] > left_difference_sum:
-            min_difference_sum = [left_difference_sum, left]
-        else:
-            break
-
-        if min_difference_sum[0] > right_difference_sum:
-            min_difference_sum = [right_difference_sum, right]
-
-    left, right = left - 1, right + 1  # 중간부터 하나씩 더해서 인덱스 이동
-print(numbers[min_difference_sum[1]])
+    # 중간에서 작은 값이 더 최소의 자연수의 합이면 출력, 아닐 경우 중간에서 큰 값을 출력
+    # 만약 같으면 작은 값이 출력
+    print(numbers[(len(numbers) // 2) - 1] if difference_number_sum((len(numbers) // 2) - 1, numbers) < difference_number_sum(len(numbers) // 2, numbers) else numbers[(len(numbers) // 2) - 1])

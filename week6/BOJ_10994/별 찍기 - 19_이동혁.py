9b6d0a7e155d93f16fd6e3d8c3471ff750f2e3eb
이동혁 <ehdgur970317@gmail.com>
2023-11-19 19:43:58 +0900

    [BOJ] 10994_별 찍기 - 19 / 실버4 / 40분 / O

---
+++ week6/BOJ_10994/별 찍기 - 19_이동혁.py
@@ -0,0 +1,71 @@
+1 - 1
+2 - 5
+3 - 9
+4 - 13
+
+n = int(input())
+
+line = 1 + 4 * (n - 1)
+
+if line == 1:
+    print('*')
+    
+else:
+    # 첫째 줄
+    for _ in range(line):
+        print('*', end = '')
+
+    print()
+
+    # 둘째 줄 ~ 중간 줄 이전까지
+    for i in range(int((line - 3) / 2)):
+        if i % 2 == 0:
+            for _ in range(int(i // 2) + 1):
+                print('* ', end = '')
+            for _ in range(int(line - (i // 2) * 2 * 2) - 3):
+                print(' ', end = '')
+            for _ in range(int(i // 2)):
+                print('* ', end = '')
+            print('*', end = '')
+            print()
+        if i % 2 == 1:
+            for _ in range(int(i//2) + 1):
+                print('* ', end = '')
+            for _ in range(int(line - (i // 2) * 2 * 2) - 4):
+                print('*', end = '')
+            print(' ', end = '')
+            for _ in range(int(i // 2)):
+                print('* ', end = '')
+            print('*', end = '')
+            print()
+            
+    # 중간 줄
+    for _ in range(int(line//2) + 1):
+        print('* ', end = '')
+    print()
+
+    # 중간 줄 이후 ~ 마지막 줄 이전까지
+    for i in range(int((line - 3) / 2), 0, - 1):
+        if i % 2 == 1:
+            for _ in range(int(i // 2) + 1):
+                print('* ', end = '')
+            for _ in range(int(line - (i // 2) * 2 * 2) - 3):
+                print(' ', end = '')
+            for _ in range(int(i // 2)):
+                print('* ', end = '')
+            print('*', end = '')
+            print()
+        if i % 2 == 0:
+            for _ in range(int(i//2)):
+                print('* ', end = '')
+            for _ in range(int(line - (i // 2) * 2 * 2) ):
+                print('*', end = '')
+            print(' ', end = '')
+            for _ in range(int(i // 2) - 1):
+                print('* ', end = '')
+            print('*', end = '')
+            print()
+
+    # 마지막 줄
+    for _ in range(line):
+        print('*', end = '')

c064bbf7be87db7f30e71938136612f9582138f9
10o0o <wotjs2923@gmail.com>
2023-12-14 11:07:31 +0900

    [BOJ] 13549 숨바꼭질 3(2) / 골드5 / 150분 / O

---
+++ week9/BOJ_13549/숨바꼭질 3_최재선(2).js
@@ -0,0 +1,74 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-bitwise */
+const fs = require('fs');
+
+const input = fs.readFileSync(
+  process.env.LOGNAME === 'jake' ? './input.txt' : '/dev/stdin',
+).toString().trim().split('\n');
+
+let [N, K] = input[0].split(' ').map(Number);
+
+if (N >= K) {
+  console.log(N - K);
+} else {
+  let result = 0;
+
+  if (N === 0) {
+    result += 1;
+    N += 1;
+  }
+
+  const getCount = (num, length) => {
+    let count = 0;
+    let compare = (2 ** (length));
+    let prefix = 2;
+
+    while (compare <= num) {
+      if (num & compare) {
+        count += prefix;
+        num -= compare;
+      }
+
+      prefix <<= 1;
+      compare <<= 1;
+    }
+
+    compare = (2 ** (length - 1));
+
+    while (num & compare) {
+      count += 1;
+      num -= compare;
+    }
+
+    while (num) {
+      while (!(num & 1)) {
+        num >>= 1;
+      }
+
+      if (num) {
+        if ((num & 2)) {
+          num += 1;
+        } else {
+          num -= 1;
+        }
+        count += 1;
+      }
+    }
+
+    return count;
+  };
+
+  let length = 1;
+  while (N < K) {
+    length += 1;
+    N <<= 1;
+  }
+
+  const num1 = N - K;
+  const num2 = K - (N >> 1);
+
+  const count1 = getCount(num1, length);
+  const count2 = getCount(num2, length - 1);
+
+  console.log(result + Math.min(count1, count2));
+}

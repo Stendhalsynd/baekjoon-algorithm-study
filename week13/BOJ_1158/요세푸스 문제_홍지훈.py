55ef8e08a3e32ddbec40cc90d05122d599761ac5
Stendhalsynd <qmflf556@naver.com>
2024-01-09 06:46:33 +0900

    [BOJ] 1158_요세푸스 문제 / 실버4 / 10분 / O

--- week13/BOJ_1158/요세푸스 문제_홍지훈.py
+++ week13/BOJ_1158/요세푸스 문제_홍지훈.py
@@ -1,1 +1,16 @@
-# 요세푸스 문제_홍지훈.py
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N, K = map(int, input().rstrip().split()) # N: 사람의 수, K: 제거할 사람의 위치
+
+nums = deque([i for i in range(1, N + 1)])
+result = []
+
+while nums:
+  nums.rotate(-(K - 1))
+  result.append(nums.popleft())
+
+result = '<' + ', '.join(map(str, result)) + '>'
+
+print(result)

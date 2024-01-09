ad6e88b3022cdaeef03933cdb4558646ca923c76
syxxne <tjdus562@naver.com>
2024-01-10 01:25:44 +0900

    [BOJ] 13335 트럭 / 실버1 / 45분 / X

---
+++ week13/BOJ_13335/트럭_이서연.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n, w, L = map(int, input().split())
+
+trucks = list(map(int, input().split()))
+
+bridge = [0] * w
+time = 0
+
+while bridge:
+    time += 1
+    bridge.pop(0)
+    
+    if trucks:
+        if sum(bridge) + trucks[0] <= L:
+            bridge.append(trucks.pop(0))
+        else:
+            bridge.append(0)
+    print(bridge)
+    
+print(time)

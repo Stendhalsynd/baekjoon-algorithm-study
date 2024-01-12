ce59edc99ffaf8ceaefb5d75e36d5d8fb549be5d
knockknock <yangban11111@gmail.com>
2024-01-12 16:43:38 +0900

    Update 트럭_양승혜.py

--- week13/BOJ_13335/트럭_양승혜.py
+++ week13/BOJ_13335/트럭_양승혜.py
@@ -2,6 +2,8 @@
 4 2 10
 7 4 5 6
 """
+# 큐 사용
+
 from collections import deque
 
 count = 0
@@ -22,3 +24,22 @@ while bridge:
 
 print(count)
     
+# 리스트 사용
+
+count = 0
+
+n, w, l = map(int, input().split())
+trains = list(map(int, input().split()))
+
+bridge = [0] * w
+
+while bridge:
+    count += 1
+    bridge.pop(0)
+    if trains:
+        if sum(bridge) + trains[0] <= l:
+            bridge.append(trains.pop(0))
+        else:
+            bridge.append(0)
+
+print(count)

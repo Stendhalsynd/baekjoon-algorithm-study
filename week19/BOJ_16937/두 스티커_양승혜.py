ea7a6beb4fc46c5621304ccf282bcd9a5147333f
knockknock <yangban11111@gmail.com>
2024-02-27 22:05:18 +0900

    두 스티커 / 실버3 / 50분 / O

--- week19/BOJ_16937/두 스티커_양승혜.py
+++ week19/BOJ_16937/두 스티커_양승혜.py
@@ -1,1 +1,23 @@
+h, w = map(int, input().split())
+stickers = []
 
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    if a <= max(h, w) and b <= max(h, w):
+        stickers.append([a, b])
+
+answer = 0
+for i in range(len(stickers)):
+    for j in range(i + 1, len(stickers)):
+        r1, c1 = stickers[i]
+        r2, c2 = stickers[j]
+        if (r1 + r2 <= h and max(c1, c2) <= w) or (max(r1, r2) <= h and c1 + c2 <= w):
+            answer = max(answer, (r1 * c1) + (r2 * c2))
+        if (r1 + c2 <= h and max(c1, r2) <= w) or (max(r1, c2) <= h and c1 + r2 <= w):
+            answer = max(answer, (r1 * c1) + (r2 * c2))
+        if (c1 + r2 <= h and max(r1, c2) <= w) or (max(c1, r2) <= h and r1 + c2 <= w):
+            answer = max(answer, (r1 * c1) + (r2 * c2))
+        if (c1 + c2 <= h and max(r1, r2) <= w) or (max(c1, c2) <= h and r1 + r2 <= w):
+            answer = max(answer, (r1 * c1) + (r2 * c2))
+
+print(answer)
